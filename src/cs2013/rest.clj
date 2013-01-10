8480772a3bab885ad581fa46a18b205bb412a47c
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-10 22:53:49 +0100

    Use query instead of post.

--- src/cs2013/rest.clj
+++ src/cs2013/rest.clj
@@ -7,7 +7,7 @@
    :remote "http://serene-spire-2229.herokuapp.com/"}) ;; git push heroku master
 
 (defn url
-  [] (:local urls))
+  [] (:remote urls))
 
 (defn query
   "Query the hdt utility."
@@ -27,8 +27,8 @@
   (query :get "?q=Est+ce+que+tu+reponds+toujours+oui(OUI/NON)")
   (query :get "?q=As+tu+bien+recu+le+premier+enonce(OUI/NON)")
   ;; ...
-  (c/post "http://localhost:5000/enonce/1" {:body "some-data"
-                                            :headers {"Content-Type" "application/x-www-form-url-encoded"}}))
+  (query :post "/enonce/1" {:body "some-data"
+                            :headers {"Content-Type" "application/x-www-form-url-encoded"}}))
 
 ;; (h/load-hook #'wikeo-query #'query #'post-cnt)
 ;; (comment (h/unload-hook #'post-cnt))
