b2a4d1e79a9a2e854f50e98d863b27163c8e4279
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-13 18:04:34 +0100

    Remove needless function

--- src/cs2013/operations.clj
+++ src/cs2013/operations.clj
@@ -55,27 +55,6 @@
                                     (recur (cons res new-acc) (first r) (next r)))
             :else                  (recur (cons c acc) (first r) (next r))))))
 
-(defn compute-operation
-  "Compute the operations (sequence of operators in chars"
-  [seq]
-  (->> seq
-       (reduce
-        (fn [acc c]
-          (cond
-           ;; parenthesis closed, we retrieve the first element which is the value, the second is the dual parenthesis (hypothesis: expression well balanced)
-           (= \) c)              (cons (first acc) (drop 2 acc))
-           ;; the operators and the opening parenthesis are simply conj-ed onto the accumulator
-           (#{\* \- \+ \/ \(} c) (cons c acc)
-           ;; 2 cases, either the first element is an operator, then we need to compute with the char c, either we just conj this car to the accumulator
-           :else                 (let [p (first acc)]
-                                   (if-let [op (operators p)]
-                                     (let [a (second acc)
-                                           val (op a c)]
-                                       (cons val (drop 2 acc)))
-                                     (cons c acc)))))
-        [])
-       first))
-
 (defn rational-2-decimal
   "Transform a rational into decimal"
   [n]
@@ -86,23 +65,4 @@
   [s]
   (-> s
       opstr-2-opdigit    ;; transforming digits char into digits
-      compute-operation  ;; computing the infix operation
-      rational-2-decimal)) ;; expected decimal and not rational
-
-;;;;;;;;;;;;;;;;;;;;;;;;;; other way
-
-(defn use-list
-  "Given a operation in string, replace parenthesis into list"
-  [l]
-  (first
-   (reduce
-    (fn [acc e]
-      (cond (#{\(} e) (cons [] acc)
-            (#{\)} e) (let [curr-list (first acc)
-                            full-acc (second acc)
-                            new-acc (drop 2 acc)]
-                        (cons (conj full-acc curr-list) new-acc))
-            :else (let [curr-list (conj (vec (first acc)) e)]
-                    (cons curr-list (rest acc)))))
-    []
-    l)))
+      rational-2-decimal)) ;; expected decimal and not rational, but integer for the rest
