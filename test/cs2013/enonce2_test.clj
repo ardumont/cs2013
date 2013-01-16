e7d9692ab93673fd9cf55e1afcc7110bcf816426
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-16 23:16:36 +0100

    Formatting

--- test/cs2013/enonce2_test.clj
+++ test/cs2013/enonce2_test.clj
@@ -4,22 +4,23 @@
         [clojure.tools.trace :only [trace] :as t]))
 
 (fact "sorting"
-      (sort-by-duration [{:VOL "META18"   :DEPART 3 :DUREE 7 :PRIX 14}
-                         {:VOL "LEGACY01" :DEPART 5 :DUREE 9 :PRIX 8}
-                         {:VOL "MONAD42"  :DEPART 0 :DUREE 5 :PRIX 10}
-                         {:VOL "YAGNI17"  :DEPART 5 :DUREE 9 :PRIX 7}]) => [{:VOL "MONAD42"  :DEPART 0 :DUREE 5 :PRIX 10}
-                                                                            {:VOL "META18"   :DEPART 3 :DUREE 7 :PRIX 14}
-                                                                            {:VOL "LEGACY01" :DEPART 5 :DUREE 9 :PRIX 8}
-                                                                            {:VOL "YAGNI17"  :DEPART 5 :DUREE 9 :PRIX 7}])
+  (sort-by-duration [{:VOL "META18"   :DEPART 3 :DUREE 7 :PRIX 14}
+                     {:VOL "LEGACY01" :DEPART 5 :DUREE 9 :PRIX 8}
+                     {:VOL "MONAD42"  :DEPART 0 :DUREE 5 :PRIX 10}
+                     {:VOL "YAGNI17"  :DEPART 5 :DUREE 9 :PRIX 7}]) => [{:VOL "MONAD42"  :DEPART 0 :DUREE 5 :PRIX 10}
+                                                                        {:VOL "META18"   :DEPART 3 :DUREE 7 :PRIX 14}
+                                                                        {:VOL "LEGACY01" :DEPART 5 :DUREE 9 :PRIX 8}
+                                                                        {:VOL "YAGNI17"  :DEPART 5 :DUREE 9 :PRIX 7}])
 
 (fact "Building a tree"
   (mktree {:dep 0 :arr 5 :prix 10}
           (mktree {:dep 5 :arr 14 :prix 7}
                   (mktree {:dep 14 :prix 7})
-                  (mktree {:dep 14 :prix 8}))) => '({:arr 5 :dep 0 :prix 10}
-                                                    ({:arr 14 :dep 5 :prix 7}
-                                                     ({:dep 14 :prix 7})
-                                                     ({:dep 14 :prix 8}))))
+                  (mktree {:dep 14 :prix 8})))
+  => '({:arr 5 :dep 0 :prix 10}
+       ({:arr 14 :dep 5 :prix 7}
+        ({:dep 14 :prix 7})
+        ({:dep 14 :prix 8}))))
 
 (fact "Build a tree from a starting point."
   (build-tree {:DEPART 0 :VOL "MONAD42"   :DUREE 5 :PRIX 10}
