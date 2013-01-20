de429ab6ebcea2a4b93976318cc539116bad9423
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-20 20:31:13 +0100

    Use its own namespace as the test has grown into something more concrete

---
+++ src/cs2013/enonce2_2.clj
@@ -0,0 +1,44 @@
+(ns ^{:doc "A namespace for testing stuff"}
+  cs2013.enonce2-2
+    (:use [midje.sweet :only [fact future-fact truthy falsey]])
+    (:require [cs2013.enonce2 :as e2]))
+
+; --------------------------------------------------------------------------------
+
+(defn in->candidates
+  "Given a list of input commands, compute the map of :path/:cmds (first level)"
+  [input]
+  (->> input
+       (map (fn [c] {:path [c] :cmds (remove #{c} input)}))))
+
+(defn valid?
+  "Are all the commands valid?"
+  [cand]
+  (every? true?
+        (map (fn [{:keys [DEPART DUREE]} next-node] (<= (+ DEPART DUREE) (:DEPART next-node)))
+             cand
+             (rest cand))))
+
+(defn candidate->commands
+  "Compute the list of children of one candidate"
+  [{:keys [path gain cmds] :as cand}]
+  (->> cmds
+       (map (fn [c] {:path (conj path c)
+                    :cmds (remove #{c} cmds)}))
+       (filter (comp valid? :path))))
+
+(defn candidates->candidates
+  "Compute all the candidates from the list of candidates"
+  [cands]
+  (->> cands
+       (mapcat candidate->commands)))
+
+(defn all-valid-commands
+  "Compute all possible matches."
+  [in]
+  (->> in
+       e2/sort-by-duration
+       in->candidates
+       (iterate candidates->candidates)
+       (take-while (comp not empty?))
+       last))
